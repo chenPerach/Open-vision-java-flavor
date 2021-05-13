53fef3825098cb7942e4812255133fbcc48fd220
chenPearch <45401724+chenPearch@users.noreply.github.com>
2021-05-13 23:17:48 +0300

    added gradle deploy task

---
+++ upload.bat
@@ -0,0 +1,2 @@
+echo UPLOADING VISION PROJECT TO RASPBERRY PI
+pscp -pw raspberry C:\Users\chen7\Documents\programmingGarbge\spring_boot\vision\build\libs\vision-0.0.1-SNAPSHOT.jar pi@raspberrypi:/home/pi/vision_project.jar 
